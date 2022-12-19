861df56ee664e5e42889dc95ce79f4272cdfd9f0
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2022-12-19 10:07:06 +0700

    fix bug build

--- app/src/main/java/com/tuanha/app/App.kt
+++ app/src/main/java/com/tuanha/app/App.kt
@@ -1,33 +1,26 @@
 package com.tuanha.app
 
-import com.one.coreapp.App
-import com.one.coreapp.Module
-import com.tuanha.app.di.appModule
-import org.koin.android.ext.android.getKoin
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
+import android.app.Application
 
-class App : App() {
-
-    private val modules by lazy {
-        getKoin().getAll<Module>()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        startKoin {
-            androidContext(this@App)
-            androidLogger(Level.NONE)
-
-            modules(
-                appModule
-            )
-        }
-
-        modules.forEach { it.init(this) }
-    }
+class App : Application() {
+//
+//    private val modules by lazy {
+//        getKoin().getAll<Module>()
+//    }
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//
+//        startKoin {
+//            androidContext(this@App)
+//            androidLogger(Level.NONE)
+//
+//            modules(
+//                appModule
+//            )
+//        }
+//
+//        modules.forEach { it.init(this) }
+//    }
 
 }
--- app/src/main/java/com/tuanha/app/di/AppModule.kt
+++ app/src/main/java/com/tuanha/app/di/AppModule.kt
@@ -1,14 +1,14 @@
-package com.tuanha.app.di
-
-import com.one.coreapp.Module
-import com.one.translate.TranslateModule
-import com.one.translate.mlkit.MlkitTranslateModule
-import org.koin.dsl.bind
-import org.koin.dsl.module
-
-val appModule = module {
-
-    single { TranslateModule() } bind Module::class
-
-    single { MlkitTranslateModule() } bind Module::class
-}
+//package com.tuanha.app.di
+//
+//import com.one.coreapp.Module
+//import com.one.translate.TranslateModule
+//import com.one.translate.mlkit.MlkitTranslateModule
+//import org.koin.dsl.bind
+//import org.koin.dsl.module
+//
+//val appModule = module {
+//
+//    single { TranslateModule() } bind Module::class
+//
+//    single { MlkitTranslateModule() } bind Module::class
+//}
