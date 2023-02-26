76d6d4486b5a2c998a91bd6065c3aacf9845df0f
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2023-02-26 22:23:16 +0700

    optimal adapter

--- core-app/src/main/java/com/one/coreapp/ui/base/adapters/BaseAsyncAdapter.kt
+++ core-app/src/main/java/com/one/coreapp/ui/base/adapters/BaseAsyncAdapter.kt
@@ -6,7 +6,6 @@ import androidx.annotation.Keep
 import androidx.recyclerview.widget.*
 import androidx.viewbinding.ViewBinding
 import com.one.coreapp.App
-import com.one.coreapp.TRANSITION
 
 abstract class BaseAsyncAdapter<T : ViewItemCloneable, B : ViewBinding>(
     itemCallback: DiffUtil.ItemCallback<T>? = null,
@@ -100,11 +99,23 @@ class BaseBindingViewHolder<B : ViewBinding>(val binding: B, val viewType: Int) 
 class DefaultItemCallback<T : ViewItem> : DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+
         return oldItem.areItemsTheSame() == newItem.areItemsTheSame()
     }
 
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
-        return false
+
+        val payloads = ArrayList<Any>()
+
+        val oldItemCompare = oldItem.getContentsCompare()
+        val newItemCompare = newItem.getContentsCompare()
+
+        oldItemCompare.forEachIndexed { index, pair ->
+
+            if (pair.first != newItemCompare.getOrNull(index)?.first) payloads.add(pair.second)
+        }
+
+        return payloads.isEmpty()
     }
 
     override fun getChangePayload(oldItem: T, newItem: T): Any? {
