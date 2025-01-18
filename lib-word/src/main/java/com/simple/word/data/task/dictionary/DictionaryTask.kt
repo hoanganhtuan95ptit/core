62dc4fcac8faa62e76d8755ad42e38648f258eed
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-01-18 09:25:04 +0700

    remove

--- lib-word/src/main/java/com/simple/word/data/task/dictionary/DictionaryTask.kt
+++
@@ -1,9 +0,0 @@
-package com.simple.word.data.task.dictionary
-
-import com.simple.task.Task
-import com.simple.word.entities.TextLevel
-
-interface DictionaryTask : Task<DictionaryTask.Param, List<TextLevel>> {
-
-    data class Param(val text: String, val inputCode: String, val outputCode: String)
-}
--- lib-word/src/main/java/com/simple/word/data/task/spelling/SpellingTask.kt
+++
@@ -1,9 +0,0 @@
-package com.simple.word.data.task.spelling
-
-import com.simple.task.Task
-import com.simple.word.entities.Spelling
-
-interface SpellingTask : Task<SpellingTask.Param, List<Spelling>> {
-
-    data class Param(val text: String, val inputCode: String)
-}
